21882b1d53f1f008e9bf386363edba0d6b09cc15
Mohannad Saker <mhndsaker@gmail.com>
2023-05-01 22:56:28 +0300

    Displaying platforms icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClients, {CanceledError} from "../services/api.clients";
 
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[]
   }
   
   interface FetchGamesResponse {
