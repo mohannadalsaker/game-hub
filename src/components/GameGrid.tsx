80e7e099fe7c337a2dfc4a010207cbaba2cfb539
Mohannad Saker <mhndsaker@gmail.com>
2023-05-02 22:45:10 +0300

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,9 +16,9 @@ const GameGrid = () => {
           sm: 1,
           md: 2,
           lg: 3,
-          xl: 4,
+          xl: 5,
         }}
-        spacing={10}
+        spacing={3}
         padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
