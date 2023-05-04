84bfbac1fe9d8d9ce9dcd26b5693ced4aea2d8a9
Mohannad Saker <mhndsaker@gmail.com>
2023-05-04 16:48:22 +0300

    Ship genres with the app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -19,6 +19,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
+
   if (isLoading) return <Spinner />;
 
   return (
